9f0ecd803b5707a50c141881291fbc3f7e260713
Nagaoka Ryunosuke <nagaokaryunosuke@E810426.local>
2023-11-05 23:04:14 +0900

    Initial commit

---
+++ src/main/java/HelloController.java
@@ -0,0 +1,8 @@
+//package com.example.helloworld;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//public class HelloController {
+//    public String hello() {
+//        return "Hello World";
+//    }
+//}
