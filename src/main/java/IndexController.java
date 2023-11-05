5f36633babff87478f957eeef3ef4245eedffbfa
Nagaoka Ryunosuke <nagaokaryunosuke@E810426.local>
2023-11-05 23:51:06 +0900

    indexページ作成

---
+++ src/main/java/IndexController.java
@@ -0,0 +1,11 @@
+//package com.example.springbootdemo;
+//
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.stereotype.Controller;
+//
+//@Controller
+//
+//public class IndexController {
+//    @RequestMapping("/index")
+//    public String index;
+//}
